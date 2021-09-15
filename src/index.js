e6f54786fc4f3ec82fb343c9c4118f3478b120a3
olyesung <olyesung@gmail.com>
2021-09-16 00:20:27 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD") {
     return count + 1;
@@ -16,10 +17,19 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
