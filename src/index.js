443ee188b1841b8c8807fd66f6da241e9a2e5412
olyesung <olyesung@gmail.com>
2021-09-16 22:21:09 +0900

    #2.2 Delete To Do

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+import { useLayoutEffect } from "react";
 import { createStore } from "redux";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
-// const ul = document.querySelector("ul");
+const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
 
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -23,11 +38,37 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    console.log(li);
+    ul.appendChild(li);
+    li.appendChild(btn);
+  });
+};
+store.subscribe(paintToDos);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
