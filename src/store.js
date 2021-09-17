415de56d5f72da6cf612a7a7df059db521ad2451
olyesung <olyesung@gmail.com>
2021-09-17 21:33:52 +0900

    #4.1 createAction

--- src/store.js
+++ src/store.js
@@ -1,36 +1,49 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+//-----------Use Redux tookit / 코드 양을 줄일수있음
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      //   const newToDoObj = { text: action.text, id: Date.now() };
-      //   return [newToDoObj, ...state];
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      //   const cleaned = state.filter((toDo) => toDo.id !== action.id);
-      //   return cleaned;
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
 };
+
+// --------- Nomal Redux
+// const ADD = "ADD";
+// const DELETE = "DELETE";
+
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
+
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id: parseInt(id),
+//   };
+// };
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [{ text: action.text, id: Date.now() }, ...state];
+//     case DELETE:
+//       return state.filter((toDo) => toDo.id !== action.id);
+//     default:
+//       return state;
+//   }
+// };
 
 const store = createStore(reducer);
 
