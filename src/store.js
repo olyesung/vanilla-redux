11362be64bd4563b50b3e8945d4160535d104d5b
olyesung <olyesung@gmail.com>
2021-09-17 22:48:59 +0900

    #4.4 createSlice

--- src/store.js
+++ src/store.js
@@ -1,32 +1,50 @@
 import { createStore } from "redux";
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  createAction,
+  createReducer,
+  createSlice,
+} from "@reduxjs/toolkit";
 
-//-----------Use Redux tookit / 코드 양을 줄일수있음
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
+//-------------🟡createSlice를 이용하여 더 줄일수있다
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = configureStore({ reducer });
+export const { add, remove } = toDos.actions;
 
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter((toDo) => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
+export default configureStore({ reducer: toDos.reducer });
+
+//-----------🟢Use Redux tookit / 코드 양을 줄일수있음
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
+
+// const reducer = createReducer([], {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
+//   [deleteToDo]: (state, action) =>
+//     state.filter((toDo) => toDo.id !== action.payload),
+// });
+
+// const store = configureStore({ reducer });
+
+// export const actionCreators = {
+//   addToDo,
+//   deleteToDo,
 // };
 
-// --------- Nomal Redux
+// export default store;
+
+// ---------🔵 Nomal Redux
 // const ADD = "ADD";
 // const DELETE = "DELETE";
 
@@ -57,9 +75,9 @@ const store = configureStore({ reducer });
 
 // const store = createStore(reducer);
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+// export const actionCreators = {
+//   addToDo,
+//   deleteToDo,
+// };
 
-export default store;
+// export default store;
